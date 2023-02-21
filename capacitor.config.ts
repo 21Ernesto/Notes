b6de7ab27d0cff7a04e60aead3eac0ec10ec215a
21Ernesto <ernestojair2020@gmail.com>
2023-02-20 22:01:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Notes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
